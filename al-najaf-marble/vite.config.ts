1b26868057174e1394c16625e12dc764b6f3e36f
MUHAMMAD Qasim JAHANGIR ATTARI <qasimjahangir25@gmail.com>
2025-06-16 04:32:37 +0500

    Update base path for GitHub Pages

--- al-najaf-marble/vite.config.ts
+++ al-najaf-marble/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/al-najaf-marble/', // 👈 IMPORTANT
+  base: "/al-najaf-marble/", // 👈 this is VERY important for GitHub Pages
 });
