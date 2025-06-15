188ba36dafddba8886867fb2fab48f72421d1267
MUHAMMAD Qasim JAHANGIR ATTARI <qasimjahangir25@gmail.com>
2025-06-16 04:42:01 +0500

    final deploy

--- al-najaf-marble/vite.config.ts
+++ al-najaf-marble/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/al-najaf-marble/", // 👈 this is VERY important for GitHub Pages
+  base: "/al-najaf-marble/", // 👈 make sure this is correct
 });
